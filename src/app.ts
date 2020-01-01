e2dab275ec5f9e86bc814b28c3859f79f3d494a5
Noer Paanakker <noer@lightworksweb.nl>
2020-01-01 17:30:10 +0100

    created base classes for project and state, implemented inheritance

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,3 @@
-// import uuidv4 from 'uuid/v4';
-
 // Validation interface
 interface FormField {
 	value: string | number;
@@ -45,13 +43,74 @@ function autobindThis(_: any, _2: string, descriptor: PropertyDescriptor) {
 	return changedDescriptor;
 }
 
+// Enum because it's a binary type
+enum ProjectStatus {
+	Active,
+	Finished,
+}
+
+type Listener<GenericState> = (items: GenericState[]) => void;
+
+// Base abstract class for Project
+abstract class BaseClass<GenericHost extends HTMLElement, GenericEl extends HTMLElement> {
+	templateEl: HTMLTemplateElement;
+	hostEl: GenericHost;
+	el: GenericEl;
+
+	constructor(templateId: string, hostElementId: string, insertAtStart: boolean, newElementId?: string) {
+		// 1. Get access to DOM elements
+		this.templateEl = document.getElementById(templateId)! as HTMLTemplateElement; // Type cast
+		this.hostEl = document.getElementById(hostElementId)! as GenericHost;
+
+		// 2. Set content to "el"
+		const importedTemplateContent = document.importNode(this.templateEl.content, true);
+		this.el = importedTemplateContent.firstElementChild as GenericEl;
+		if (newElementId) {
+			this.el.id = newElementId;
+		}
+
+		this.attachContent(insertAtStart);
+	}
+
+	private attachContent(insertAtStart: boolean) {
+		this.hostEl.insertAdjacentElement(insertAtStart ? 'afterbegin' : 'beforeend', this.el);
+	}
+
+	// abstract methods so other devs know to create in inherited class
+	abstract configure?(): void;
+	abstract renderContent(): void;
+}
+
+// Base abstract class for state
+class BaseState<GenericState> {
+	protected listeners: Listener<GenericState>[] = [];
+
+	constructor() {}
+
+	public addListener(listenerFn: Listener) {
+		this.listeners.push(listenerFn);
+	}
+}
+
+// Class to type cast Project
+class Project {
+	public constructor(
+		public id: string,
+		public title: string,
+		public desc: string,
+		public people: number,
+		public status: ProjectStatus
+	) {}
+}
+
 // Class to hold application state, singleton class
-class ProjectState {
-	private listeners: any[] = [];
-	private projects: any[] = [];
+class ProjectState extends BaseState<Project> {
+	private projects: Project[] = [];
 	private static instance: ProjectState;
 
-	private constructor() {}
+	private constructor() {
+		super();
+	}
 
 	static getInstance() {
 		// Make it into singleton class
@@ -62,19 +121,9 @@ class ProjectState {
 		return this.instance;
 	}
 
-	public addListener(listenerFn: Function) {
-		this.listeners.push(listenerFn);
-	}
-
 	// Adds a new project to state
 	public addProject(title: string, desc: string, numOfPeople: number) {
-		const newProject = {
-			// id: uuidv4(),
-			id: Math.random().toString(),
-			title,
-			desc,
-			people: numOfPeople,
-		};
+		const newProject = new Project(Math.random().toString(), title, desc, numOfPeople, ProjectStatus.Active);
 		this.projects.push(newProject);
 
 		// Execute correct listener function with projects array
@@ -88,38 +137,24 @@ class ProjectState {
 const projectState = ProjectState.getInstance();
 
 // Class to store created projects
-class ProjectList {
-	templateEl: HTMLTemplateElement;
-	hostEl: HTMLDivElement;
-	projectEl: HTMLElement;
-	assignedProjects: any[];
+class ProjectList extends BaseClass<HTMLDivElement, HTMLElement> {
+	assignedProjects: Project[];
 
 	constructor(private projectType: 'active' | 'finished') {
+		super('project-list', 'app', false, `${projectType}-projects`);
+
 		this.assignedProjects = [];
 
-		// 1. Get access to DOM elements
-		this.templateEl = document.getElementById('project-list')! as HTMLTemplateElement; // Type cast
-		this.hostEl = document.getElementById('app') as HTMLDivElement;
-
-		// 2. Set content to "el"
-		const importedTemplateContent = document.importNode(this.templateEl.content, true);
-		this.projectEl = importedTemplateContent.firstElementChild as HTMLElement;
-		this.projectEl.id = `${this.projectType}-projects`;
-
-		projectState.addListener((projects: any[]) => {
-			this.assignedProjects = projects;
-			this.renderProjects();
-		});
-
 		// 3. Inject content into DOM
-		this.attachContent();
+
+		this.configure();
 		this.renderContent();
 	}
 
 	private renderProjects() {
 		// 1. Access list in template
 		const listEl = document.getElementById(`${this.projectType}-projects-list`)! as HTMLUListElement;
-
+		listEl.innerHTML = '';
 		// 2. Add each project to the list in DOM
 		for (const project of this.assignedProjects) {
 			const listItem = document.createElement('li');
@@ -128,48 +163,43 @@ class ProjectList {
 		}
 	}
 
-	private renderContent() {
-		const listId = `${this.projectType}-projects-list`;
-		// Set element id
-		this.projectEl.querySelector('ul')!.id = listId;
-		// Inject content for header
-		this.projectEl.querySelector('h2')!.textContent = this.projectType.toUpperCase() + ' PROJECTS';
+	configure() {
+		projectState.addListener((projects: Project[]) => {
+			const relevantProjects = projects.filter(prj => {
+				if (this.projectType === 'active') {
+					return prj.status === ProjectStatus.Active;
+				}
+				return prj.status === ProjectStatus.Finished;
+			});
+			this.assignedProjects = relevantProjects;
+			this.renderProjects();
+		});
 	}
 
-	private attachContent() {
-		this.hostEl.insertAdjacentElement('beforeend', this.projectEl);
+	renderContent() {
+		const listId = `${this.projectType}-projects-list`;
+		// Set element id
+		this.el.querySelector('ul')!.id = listId;
+		// Inject content for header
+		this.el.querySelector('h2')!.textContent = this.projectType.toUpperCase() + ' PROJECTS';
 	}
 }
 
 // Class for form to create new projects
-class ProjectForm {
+class ProjectForm extends BaseClass<HTMLDivElement, HTMLFormElement> {
 	// Annotate types
-	templateEl: HTMLTemplateElement;
-	hostEl: HTMLDivElement;
-	formEl: HTMLFormElement;
 	titleInputEl: HTMLInputElement;
 	descInputEl: HTMLInputElement;
 	peopleInputEl: HTMLInputElement;
 
 	public constructor() {
-		// 1. Get access to DOM elements
-		this.templateEl = document.getElementById('project-input')! as HTMLTemplateElement; // Type cast
-		this.hostEl = document.getElementById('app') as HTMLDivElement;
+		super('project-input', 'app', true, 'user-input');
 
-		// 2. Set content to "el"
-		const importedTemplateContent = document.importNode(this.templateEl.content, true);
-		this.formEl = importedTemplateContent.firstElementChild as HTMLFormElement;
-		this.formEl.id = 'user-input';
+		this.titleInputEl = this.el.querySelector('#title') as HTMLInputElement;
+		this.descInputEl = this.el.querySelector('#description') as HTMLInputElement;
+		this.peopleInputEl = this.el.querySelector('#people') as HTMLInputElement;
 
-		// 3. Get and set to form input fields
-		this.titleInputEl = this.formEl.querySelector('#title') as HTMLInputElement;
-		this.descInputEl = this.formEl.querySelector('#description') as HTMLInputElement;
-		this.peopleInputEl = this.formEl.querySelector('#people') as HTMLInputElement;
-
-		this.submitForm();
-
-		// 4. Attach new content to DOM
-		this.attachContent();
+		this.configure();
 	}
 
 	private gatherUserInput(): [string, string, number] | void {
@@ -212,13 +242,11 @@ class ProjectForm {
 		}
 	}
 
-	private submitForm() {
-		this.formEl.addEventListener('submit', this.formSubmitHandler);
-	}
+	renderContent() {}
 
-	private attachContent = () => {
-		this.hostEl.insertAdjacentElement('afterbegin', this.formEl);
-	};
+	public configure() {
+		this.el.addEventListener('submit', this.formSubmitHandler);
+	}
 }
 
 const projForm = new ProjectForm();
